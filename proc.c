9aafec81216278b04235d7b124a5f14be712f101
rsohi001 <rsohi001@ucr.edu>
2020-01-28 03:10:03 +0000

    Changes to proc.c; added function waitpid().

--- proc.c
+++ proc.c
@@ -269,7 +269,7 @@ exit(int)
 
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
-int
+int 
 wait(void)
 {
   struct proc *p;
@@ -297,6 +297,52 @@ wait(void)
         p->state = UNUSED;
         release(&ptable.lock);
         return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+int
+waitpid(int pid, int *status, int options)
+{
+  struct proc *p;
+  int havekids, pid2;
+  struct proc *curproc = myproc();
+
+ if(options == 3){ // Placeholder for WNOHANG, if options is set to 3 we will run WNOHANG FUNCTION
+
+
+ }
+
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == pid){
+        // Found one.
+        pid2 = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid2 = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid2;
       }
     }
 
